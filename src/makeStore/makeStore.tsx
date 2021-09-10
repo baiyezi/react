f2c5d1f2a1ee3e165fb8fb39bfa767a0603f8033
662 <lya121@gmail.com>
2021-09-10 14:39:46 +0800

    fix: Update comments for makeStore

--- src/makeStore/makeStore.tsx
+++ src/makeStore/makeStore.tsx
@@ -1,16 +1,26 @@
 import React from 'react'
 
 export interface ProviderProps<T> {
+  /**
+   * Store value
+   */
   value?: T
 }
 
+/**
+ * Make a context store and context provider
+ * @param defaultValue The default value
+ * @returns Provider and react hook
+ */
 export default function makeStore<T>(defaultValue: T) {
   const context = React.createContext<
     [T, React.Dispatch<React.SetStateAction<T>>]
   >([defaultValue, () => {}])
 
-  // 可以通过 value 重新设 store 的值
-  // store 默认为 defaultValue
+  /**
+   * Context store provider
+   * Can be reset store value
+   */
   const Provider: React.FC<ProviderProps<T>> = ({
     value = defaultValue,
     children,
